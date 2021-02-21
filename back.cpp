dbb1b5402376dbd3a049b9f8809c7c3999310113
Zain Ul Abideen <48854250+ilprofessore@users.noreply.github.com>
2021-02-21 11:33:31 +0500

    Update back.cpp

--- back.cpp
+++ back.cpp
@@ -4,6 +4,6 @@ void byebye()
 {
     system("cls");
     std::cout << "\n\tCONGRATULATIONS ON FINISHING THE GAME.\n\tMORE LEVELS WILL BE CREATED SOON.\n";
-    std::cout << "\n\tfollow me on\n\tinstagram: @__ilprofessore\n\temail: msfmapper@gmail.com\n\n";
+    std::cout << "\n\tfollow me on\n\tinstagram: @spqr__ilprofessore\n\temail: msfmapper@gmail.com\n\n";
     getchar();
 }
